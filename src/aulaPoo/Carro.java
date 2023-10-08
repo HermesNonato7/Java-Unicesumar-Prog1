1851bde7ef11ca75f0367b433605efff171bb305
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-10-07 22:05:52 -0300

    adicionando aula POO add classes e metodos novos

--- src/aulaPoo/Carro.java
+++ src/aulaPoo/Carro.java
@@ -5,19 +5,71 @@ package aulaPoo;
  * @author HermesNonato7
  */
 public class Carro {
-    
-    public String modelo;
-    
-    public int qntPortas;
-    
-    public String placa;
-    
-    public void acelerar(){
+
+    private String modelo;
+
+    private int qntPortas;
+
+    private String placa;
+
+    private String cor;
+
+    private String combustivel;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getQntPortas() {
+        return qntPortas;
+    }
+
+    public void setQntPortas(int qntPortas) {
+        this.qntPortas = qntPortas;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public void ligar() {
+        System.out.println("Ligado");
+    }
+
+    public void desligar() {
+        System.out.println("Desligado");
+    }
+
+    public void acelerar() {
         System.out.println("Acelerando");
     }
-    
-    public void parar(){
+
+    public void parar() {
         System.out.println("Parando");
     }
-    
+
 }
--- src/aulaPoo/PrincipalCarro.java
+++ src/aulaPoo/PrincipalCarro.java
@@ -5,17 +5,19 @@ package aulaPoo;
  * @author HermesNonato7
  */
 public class PrincipalCarro {
-    
-    public static void main(String args[]){
-       
+
+    public static void main(String args[]) {
+
         Carro obj = new Carro();
-        
-        obj.modelo = "BMW X!";
-        obj.placa = "QWE 1234";
-        obj.qntPortas = 4;
-        
+
+        obj.setModelo("BMW X1");
+        obj.setQntPortas(4);
+        obj.setPlaca("QWE 1234");
+        obj.setCor("Azul Royal");
+        obj.setCombustivel("gasolina");
+
         obj.acelerar();
-        
+
     }
-    
+
 }
