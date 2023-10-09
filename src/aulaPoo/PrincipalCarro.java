36bd3d9427daac2d7fb48124fed47333ccef2832
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-10-09 20:25:26 -0300

    adicionando código - aula POO

--- src/aulaPoo/PrincipalCarro.java
+++ src/aulaPoo/PrincipalCarro.java
@@ -19,6 +19,28 @@ public class PrincipalCarro {
 
         obj.acelerar();
 
+        Carro obj2 = new Carro();
+
+        obj2.setModelo("Fiat");
+        obj2.setQntPortas(5);
+        obj2.setPlaca("JNH 1234");
+        obj2.setCor("Vermelho");
+        obj2.setCombustivel("Flex");
+        obj2.setMotor(1.5f);
+
+        obj.acelerar();
+
+        // Comparando objetos
+        System.out.println(obj + " <=> " + obj2);
+
+        // Equals serve para comparar valores de atributos de objetos diferentes
+        if (obj.getPlaca().equals(obj2.getPlaca())) {
+            System.out.println("O objeto é igual");
+        } else {
+            System.out.println("O objeto é diferente");
+
+        }
+
     }
 
 }
